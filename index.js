79b0fbf1ee9136326d496a4da55604c49d7a19cc
Justin Reyes <justin.regi.reyes@gmail.com>
2016-01-18 21:30:30 -0800

    fixed the file serve

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // require modules
 var express = require('express'),
-	logger = require('morgan');
+	logger = require('morgan'),
+	path = require('path');
 
 // create instance of express
 var app = express();
@@ -11,9 +12,21 @@ var port = (process.env.PORT || 3000);
 // middleware
 app.use(logger('dev'));
 
-app.use('./api.js');
+// save views into variable
+var views = path.join(process.cwd(), 'views/');
 
+// require routes
+var api = require('./api');
 
+// serve js and css files
+app.use('/static', express.static('public'));
+
+// routes
+app.use(api);
+
+app.get(["/", "*"], function(req, res) {
+	res.sendFile(path.join(views, 'application.html'));
+});
 
 app.listen(port, function() {
 	console.log('There is free food at port ' + port);
