a93dc1b59be6ed9c6e57a92b66a14e75a367d5c6
Justin Reyes <justin.regi.reyes@gmail.com>
2016-01-28 11:46:58 -0800

    cleaned up unused code

--- api.js
+++ api.js
@@ -1,4 +1,9 @@
 var express = require('express');
 var api = express.Router();
+var sessionCtrl = require('./controllers/session_controller.js');
+var twitCtrl = require('./controllers/twit_controller.js');
+var env = process.env;
+
+
 
 module.exports = api;
