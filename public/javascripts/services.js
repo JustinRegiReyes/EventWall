892c628788f84430b848d9468689adb754e83a30
Justin Reyes <justin.regi.reyes@gmail.com>
2016-01-29 00:21:52 -0800

    added AuthService factory

--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -1,1 +1,131 @@
 var app = angular.module('mediaWall.services', []);
+
+app.factory('AuthService', function ($q, $timeout, $http, $window) {
+
+  // create user variable
+  var user = $window.user || null;
+
+  // return available functions for use in controllers
+  return ({
+    isLoggedIn: isLoggedIn,
+    getUserStatus: getUserStatus,
+    login: login,
+    logout: logout,
+    register: register,
+    twitterOAuth: twitterOAuth
+  });
+
+  function isLoggedIn() {
+      return !!user;
+  }
+
+  function getUserStatus() {
+    return user;
+  }
+
+  function login(username, password) {
+
+    // create a new instance of deferred
+    var deferred = $q.defer();
+
+    // send a post request to the server
+    $http.post('/api/user/login', {username: username, password: password})
+      // handle success
+      .success(function (res, status) {
+        if(status === 200 && res.data){
+          user = res.data;
+          deferred.resolve();
+        } else {
+          user = false;
+          deferred.reject();
+        }
+      })
+      // handle error
+      .error(function (res) {
+        user = null;
+        deferred.reject();
+      });
+
+    // return promise object
+    return deferred.promise;
+
+  }
+
+  function logout() {
+
+    // create a new instance of deferred
+    var deferred = $q.defer();
+
+    // send a get request to the server
+    $http.get('/api/user/logout')
+      // handle success
+      .success(function (res) {
+        user = null;
+        deferred.resolve();
+      })
+      // handle error
+      .error(function (res) {
+        user = null;
+        deferred.reject();
+      });
+
+    // return promise object
+    return deferred.promise;
+
+  }
+
+  function register(username, password) {
+
+    // create a new instance of deferred
+    var deferred = $q.defer();
+
+    // send a post request to the server
+    $http.post('/api/user/register', {username: username, password: password})
+      // handle success
+      .success(function (res, status) {
+        if(status === 200 && res.data){
+          user = res.data;
+          deferred.resolve();
+        } else {
+          deferred.reject();
+        }
+      })
+      // handle error
+      .error(function (res) {
+        deferred.reject();
+      });
+
+    // return promise object
+    return deferred.promise;
+
+  }
+
+  function twitterOAuth() {
+  	delete $http.defaults.headers.common['X-Requested-With'];
+
+    // create a new instance of deferred
+    var deferred = $q.defer();
+
+    // send a post request to the server
+    $http.get('/auth/twitter')
+      // handle success
+      .success(function (res, status) {
+        if(status === 200 && res.data){
+          user = res.data;
+          deferred.resolve();
+        } else {
+          deferred.reject();
+        }
+      })
+      // handle error
+      .error(function (res) {
+      	console.log('Error');
+        deferred.reject();
+      });
+
+    // return promise object
+    return deferred.promise;
+
+  }
+
+});
