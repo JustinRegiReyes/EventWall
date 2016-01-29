2253f6cc46d74dab5301de1a6a9c0e0c28572220
Justin Reyes <justin.regi.reyes@gmail.com>
2016-01-29 00:14:24 -0800

    added routes for user

--- api.js
+++ api.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var api = express.Router();
+var usersCtrl = require('./controllers/users_controller.js');
 var sessionCtrl = require('./controllers/session_controller.js');
 var twitCtrl = require('./controllers/twit_controller.js');
 var env = process.env;
+
+// api routes
+
+// users & sessions
+api.post('/api/user/register', usersCtrl.register)
+api.post('/api/user/login', usersCtrl.login)
+api.get('/api/user/logout', usersCtrl.logout)
 
 
 
