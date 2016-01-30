7e8fa44528d9f041e37a12efcec36e0ec28e18d8
Justin Reyes <justin.regi.reyes@gmail.com>
2016-01-30 14:01:37 -0800

    flashMessages are set back to null after being sent, if there are any

--- routes.js
+++ routes.js
@@ -9,9 +9,9 @@ var express = require('express'),
     public = path.join(process.cwd(), 'public/'),
     templates = path.join(public, 'templates/');
 
+var flashMessages = {};
 var app = require('./index.js');
 
-var flashMessages = {};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,11 +26,13 @@ exports.templates = function(req, res) {
   var filename = req.params.filename.toString();
   filename += '.ejs';
 
+  var currentUser = req.user;
+
   if(!filename) return;  // might want to change this
 
-  console.log('app.locals', flashMessages);
-  res.render(path.join(templates, filename), {message: 'hello', flashMessages: flashMessages} );
-  flashMessages.flashError = null;
+  res.render(path.join(templates, filename), {user: currentUser, flashMessages: flashMessages});
+  flashMessages.noTwitter = null;
+  flashMessages.duplTwitter = null;
 };
 
 exports.index = function(req, res) {
@@ -39,10 +41,11 @@ exports.index = function(req, res) {
 };
 
 exports.fallback = function(req, res) {
-	// console.log(req.flash('error'));
-	var flashError = req.flash('error');
-	if(flashError.length > 0) {
-		flashMessages.flashError = flashError;
-	}
-  res.render(path.join(views, 'application.html.ejs'), {user: req.user, test: 'test'});
+	var duplTwitter = req.flash('duplTwitter');
+	var noTwitter = req.flash('noTwitter');
+	
+	console.log('req.user check', req.user);
+	flashMessages.noTwitter = noTwitter ? noTwitter : null;
+	flashMessages.duplTwitter = duplTwitter ? duplTwitter : null;
+  res.render(path.join(views, 'application.html.ejs'), {user: req.user});
 }
