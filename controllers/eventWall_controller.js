e9076b1760c24008aa45d4a2e4db778368fb86f7
Justin Reyes <justin.regi.reyes@gmail.com>
2016-02-09 10:22:33 -0800

    removed unusable code

--- controllers/eventWall_controller.js
+++ controllers/eventWall_controller.js
@@ -2,12 +2,13 @@ var express = require('express'),
     passport = require('passport'),
     app = require('.././index.js'),
     User = require('./../models').User,
-	EventWall = require('./../models').EventWall;
+	EventWall = require('./../models').EventWall,
+	Twit = require('twit');
 
 module.exports.create = function(req, res) {
 	var eventWallData = req.body;
 
-	//find logged in user
+	//find logged in user to edit in DB
 	req.currentUser(function(err, user) {
 		if(err) {return console.log(err)};
 
@@ -31,33 +32,39 @@ module.exports.get = function(req, res) {
 	console.log('eventWallUrl', eventWallUrl);
 
 	EventWall.findByUrl(eventWallUrl, function(err, eventWall) {
-	if(err) {
-		return res.status(500).json({err: 'An error has occured. Please refresh the page.'});
-	}
+		if(err) {
+			return res.status(500).json({err: 'An error has occured. Please refresh the page.'});
+		}
 
-	if(eventWall === null) {
-		return res.status(404).json({err: 'An event with this Url does not exist.'});
-	}
+		if(eventWall === null) {
+			return res.status(404).json({err: 'An event with this Url does not exist.'});
+		}
 
-	return res.status(200).json({data: eventWall});
+		return res.status(200).json({data: eventWall});
 
-});
-
-	//find logged in user
-	// req.currentUser(function(err, user) {
-	// 	if(err) {return console.log(err)};
-
-	// 	EventWall.create(eventWallData, function(err, eventWall) {
-	// 		if(err) { console.log('errr?'); return res.status(500).json({err: err})}
-
-	// 		// console.log('userLoggedIn?', !!req.user);
-	// 		eventWall.addToUser(user, function(user) {
-	// 			req.login(user, function(err) {
-	// 				console.log('updated logged in user!');
-	// 				res.status(200).json({data: eventWall});
-	// 			})
-	// 		});
-	// 	})
-	// });
-	
+	});
 };
+
+module.exports.feed = function(req, res) {
+	var eventWallUrl = req.query.eventWallUrl;
+	// req.user is user info. Not able to manipulate in DB
+	var user = req.user;
+
+	var T = new Twit({
+	  consumer_key:         'u1AQm3v8qwIOHho9znwgm2SJ8',
+	  consumer_secret:      '87ewdo1HQp6yPeNTqYlAm6fXmlBFrAIrRdHBSuOzKuEawgBm0u',
+	  access_token:         '2694541230-FqQmbhi0N2hcWlvbAIlnECrCPa7lqL0duNDSHUm',
+	  access_token_secret:  'oGcDSQNQfHGWWOYWYxAXFJEVNwDtKwOofb60HvsHMjpjF',
+	  timeout_ms:           60*1000
+	})
+
+	T.get('search/tweets', { q: 'lisafoundthejuan OR #the100 -rt', count: 10 , lang: 'en'}, function(err, data, response) {
+		// console.log('data', data);
+		data.statuses.forEach(function(status) {
+			console.log('---------------');
+			status.type = 'twitter';
+			console.log(status);
+		})
+	  return res.status(200).json({data: data.statuses});
+	})
+}
