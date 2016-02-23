ae0991e5be3d00b049ee7f526f1afaa009301130
Justin Reyes <justin.regi.reyes@gmail.com>
2016-02-23 13:20:54 -0800

    centered Event Walls list

--- seed.js
+++ seed.js
@@ -5,19 +5,19 @@ var db = require("./models");
 // });
 
 var post = {text: 'test3'};
-var eventWallIds = [ '56bda6eee28cb22519832b94',
-     '56be44e0d657389451ae241a',
-     '56be45218a978151520a5337',
-     '56be459d6be255fa5245370b',
-     '56be5196e95b42e0618271a6',
-     '56be92e23bac4290b6066824',
-     '56be93b43bac4290b6066825' ];
+// var eventWallIds = [ '56bda6eee28cb22519832b94',
+//      '56be44e0d657389451ae241a',
+//      '56be45218a978151520a5337',
+//      '56be459d6be255fa5245370b',
+//      '56be5196e95b42e0618271a6',
+//      '56be92e23bac4290b6066824',
+//      '56be93b43bac4290b6066825' ];
 
- db.EventWall.find({ _id: {$in: eventWallIds}
-}, function(err, eventWalls) {
- 	console.log(eventWalls);
- 	process.exit()
- });
+//  db.EventWall.find({ _id: {$in: eventWallIds}
+// }, function(err, eventWalls) {
+//  	console.log(eventWalls);
+//  	process.exit()
+//  });
 
 // db.EventWall.update({url: 'cats'}, {$push: {posts: post}},
 // function(err, eventWall) {
@@ -28,7 +28,7 @@ var eventWallIds = [ '56bda6eee28cb22519832b94',
 // 	});
 // });
 
-// db.EventWall.update({url: 'lisafoundthejuan'}, {bannedPosts: new Array},
+// db.EventWall.update({url: 'lisafoundthejuan'}, {url: 'LisaFoundTheJuan'},
 // function(err, eventWall) {
 // 	db.EventWall.findOne({url: 'lisafoundthejuan'}, function(err, neweventWall) {
 // 		if(err) {return console.log(err)}	
@@ -37,14 +37,18 @@ var eventWallIds = [ '56bda6eee28cb22519832b94',
 // 	});
 // });
 
-// db.EventWall.findOne({url: 'lisafoundthejuan'}, function(err, eventWall) {
+// db.EventWall.findOne({url: { $regex: 'theflashtv', $options: 'i' }}, function(err, eventWall) {
 // 	console.log(eventWall);
 // 	process.exit();
 // });
 
-// 	console.log(eventWalls);
+// db.EventWall.findOne({url: 'LISAFOUNDTHEJUAN'}, function(err, eventWall) {
+// 	if(err) {
+// 		console.log(err);
+// 	}
+// 	console.log(eventWall);
 // 	process.exit();
-// })
+// });
 
 // db.Poster.collection.drop(function() {
 // 	process.exit();
